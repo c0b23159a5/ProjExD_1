bd8e00e72522421dfcffcd864b2e607383e58cbb
c0b23159a5 <c0b23159a5@edu.teu.ac.jp>
2024-11-19 16:34:48 +0900

    update 練習問題 6 add background_image_frame_move fix ワープ

--- pygame_image.py
+++ pygame_image.py
@@ -11,15 +11,21 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     tmr = 0
+    # bg_width 800
+    bg_img1 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        background_image_frame_move = -(tmr%800)
-        print(background_image_frame_move)
+
+        background_image_frame_move = -(tmr%3200)
         screen.blit(bg_img, [background_image_frame_move, 0])
+        screen.blit(bg_img1, [background_image_frame_move+1600, 0])
+        screen.blit(bg_img, [background_image_frame_move+3200, 0])
+        screen.blit(bg_img1, [background_image_frame_move+4800, 0])
+
         screen.blit(kk_img, [300, 200])
 
         pg.display.update()
