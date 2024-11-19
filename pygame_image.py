7c749de58900822dd3bc68c4d07658d9cd4b4d29
c0b23159a5 <c0b23159a5@edu.teu.ac.jp>
2024-11-19 15:35:29 +0900

    update 練習問題１,2

--- pygame_image.py
+++ pygame_image.py
@@ -11,11 +11,16 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     tmr = 0
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [0, 0])
+
         pg.display.update()
         tmr += 1        
         clock.tick(10)
